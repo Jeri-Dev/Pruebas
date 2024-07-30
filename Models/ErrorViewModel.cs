475d1a097eb4c948e8300bdafd17debd083fd445
Jeriel Gomez <jgomezinf19@gmail.com>
2024-07-29 22:37:18 -0400

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cajero_automatico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
